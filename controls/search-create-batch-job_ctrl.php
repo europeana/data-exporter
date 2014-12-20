5261c8c20f95f90199f7d918556b9d152f2abe03
dan-nl <d_entous@yahoo.com>
2014-12-20 05:30:43 +0100

    corrected the text & link for the no cookie message for the search request forms.

--- controls/search-create-batch-job_ctrl.php
+++ controls/search-create-batch-job_ctrl.php
@@ -59,7 +59,7 @@
 
 			// check for cookie
 			if ( !$Session->cookiePresent() ) {
-				$html_result .= '<ul><li><span class="error">In order to use this form, your browser must accept cookies for this site.</span></li><li><a href="https://support.google.com/websearch/answer/35851?hl=en" target="_external">Enable cookies</a> for this site and then return to <a href="/my-europeana/tag-list-search">the tag list search form</a>.</li></ul>';
+				$html_result .= '<ul><li><span class="error">In order to use this form, your browser must accept cookies for this site.</span></li><li><a href="https://support.google.com/websearch/answer/35851?hl=en" target="_external">Enable cookies</a> for this site and then return to <a href="/search">the search form</a>.</li></ul>';
 				$html_result .= $empty_result;
 				break;
 			}
