bc245d7aa06a95b9261a7fcffa84053d223aec00
dan-nl <d_entous@yahoo.com>
2014-12-31 09:27:17 +0100

    setup for queue deletion

--- controls/queue/delete_ctrl.php
+++ controls/queue/delete_ctrl.php
@@ -5,9 +5,9 @@
 	 */
 	header( 'Content-Type: ' . $config['content-type'] . '; charset=' . $config['charset'] );
 
-	$Page->page = 'queue/';
-	$Page->title = 'Queue: ' . $config['site-name'];
-	$Page->heading = 'Queue: ' . $config['site-name'];
+	$Page->page = 'queue/delete';
+	$Page->title = 'Delete, Queue: ' . $config['site-name'];
+	$Page->heading = 'Delete, Queue: ' . $config['site-name'];
 	$Page->view = 'html-layout_tpl.php';
 
 
@@ -19,21 +19,12 @@
 
 	try {
 
-		$job_path = realpath( APPLICATION_PATH . '/cli-jobs/' ) . '/';
-
-		$html = App\Helpers\Jobs::retrieveJobsAsHtmlTable(
-			array(
-				'filename' => $config['dataset-jobs'],
-				'path' => $job_path
-			)
-		);
 
 	} catch( Exception $e ) {
 
 		$html .= '<p class="error">' . $e->getMessage() . '</p>';
 
 	}
-
 
 	/**
 	 * set-up page view
