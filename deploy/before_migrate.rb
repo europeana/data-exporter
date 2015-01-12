1fe6abd5d441fe7ff6584e0db4d9e418b69c61ca
dan-nl <d_entous@yahoo.com>
2015-01-12 05:24:16 +0100

    adjusted deploy hook

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -2,7 +2,9 @@
 run "ln -nfs #{config.shared_path}/config/config.ini #{config.release_path}/config/config.ini"
 
 # cli-jobs
+run "mkdir -p #{config.shared_path}/cli-jobs"
 run "ln -nfs #{config.shared_path}/cli-jobs #{config.release_path}/cli-jobs"
 
 # cli-jobs-completed
+run "mkdir -p #{config.shared_path}/cli-jobs-completed"
 run "ln -nfs #{config.shared_path}/cli-jobs-completed #{config.release_path}/cli-jobs-completed"
